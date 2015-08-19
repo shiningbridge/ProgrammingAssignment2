248dd0da6094051172f7f6c3e6f10b141944fe46
kangqiao <zhaokangqiao@gmail.com>
2015-08-20 00:08:19 +0200

    add comments and explain the 'x' special matrix

--- cachematrix.R
+++ cachematrix.R
@@ -8,16 +8,23 @@
 # `makeCacheMatrix`: This function creates a special "matrix" object
 #     that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-  iM <- NULL
+  iM <- NULL # iM is the value to be cached
   set <- function(y){
+    # set the special matrix value into cache
     x <<- y
+    # in cache remove the old cached value
+    # by set the value to NULL, since the original
+    # matrix has been changed.
     iM <<- NULL
   }
   get <- function() x
+  # pass the inverted matrix 'invMatrix' to cache
   setInv <- function(invMatrix) iM <<- invMatrix
+  # get inverted Matrix
   getInv <- function() iM
 
-  # return the inverted matrix
+  # return the functions list which can be seen as
+  # a special Matrix
   list(set = set, get = get,
        setInv = setInv,
        getInv = getInv)
@@ -29,14 +36,25 @@ makeCacheMatrix <- function(x = matrix()) {
 #     already been calculated (and the matrix has not changed), then
 #     `cacheSolve` should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
+  # x is computed by 'makeCacheMatrix' function
+  # iM in cache is either 'NULL'
+  # or cached value that invert the special matrix
   iM <- x$getInv()
+  # if iM is NOT 'NULL'
   if (!is.null(iM)) {
     message("getting caching inverted Matrix")
+    # return explicitly the cached value. Fast and easy!
     return(iM)
   }
+  # Otherwise the iM has not been calculated before
+  # or has been set to 'NULL' by set() function defined in makeCacheMatrix()
   Mat <- x$get()
+  # Real calculation should happen.
   iM <- inverse(Mat,...)
+  # Now put the iM into cache
   x$setInv(iM)
   ## Return a matrix that is the inverse of 'x'
+  # this 'x' is the special matrix that returned from
+  # makeCacheMatrix() function, in fact, a list.
   iM
 }
